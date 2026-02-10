fc02a97be4e0753ff6cc9f6de974945f20c323ad
iit2022231-sys <iit2022231@iiita.ac.in>
2026-02-10 20:58:46 +0530

    dm_promotion_group_service_add_more_groups_messages_forget_some_service_run

--- telegram_bot_module/main.py
+++ telegram_bot_module/main.py
@@ -59,8 +59,8 @@ class TelegramBot:
         
         try:
 
-            # posts = await self.fetcher.fetch_all()
-            # refined_posts = self.ai_service.refine_posts(posts)
+            posts = await self.fetcher.fetch_all()
+            refined_posts = self.ai_service.refine_posts(posts)
 
             # with open("refined_posts.txt", "w", encoding="utf-8") as f:
             #     for original, refined in zip(posts, refined_posts):
@@ -70,14 +70,14 @@ class TelegramBot:
             #         f.write(refined.text + "\n")
             #         f.write("-" * 40 + "\n")
 
-            # await self.distributor_channels.send_posts()
+            await self.distributor_channels.send_posts()
 
-            # if posts:
-            #     await self.distributor.send_posts(refined_posts)
-            # else:
-            #     self.logger.info("No posts to distribute")
+            if posts:
+                await self.distributor.send_posts(refined_posts)
+            else:
+                self.logger.info("No posts to distribute")
             
-            # await self.dm_promotion_service.run()
+            await self.dm_promotion_service.run()
             await self.group_promotion_service.run()
             
             return True
